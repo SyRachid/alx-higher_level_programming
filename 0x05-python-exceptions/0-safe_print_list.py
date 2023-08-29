ac2c85a380f00f6edf37facd4c1901086502efc0
SyRachid <syrachidsanogo78@gmail.com>
2023-08-29 01:43:55 +0000

    new commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-def safe_print_list(my_list = [], x = 0):
-	try:
-	    i = 0
-	    outpout = ""
-	    for element in my_list:
-		if i < x:
-		   outpout += str(element)
-		   i += 1
-	    print(outpout)
-	    return i
-	except:
-	       return i
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        output = ""
+        for element in my_list:
+            if count < x:
+                output += str(element)
+                count += 1
+        print("{}".format(output))
+        return count
+    except:
+        return count
