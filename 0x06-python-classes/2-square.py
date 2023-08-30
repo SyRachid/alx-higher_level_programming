ebcf639ed4dc5f1666987227f85b38b9fc1d16fb
SyRachid <syrachidsanogo78@gmail.com>
2023-08-30 00:21:03 +0000

    fixing some bugs in the code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """constructor of  my class"""
         self.__size = size
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be in a integer")
-        if (size < 0):
+        if size < 0:
             raise ValueError("size must be >=0 ")
