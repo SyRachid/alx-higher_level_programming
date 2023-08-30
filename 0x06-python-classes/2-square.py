3cbc2c223d71c6305ecdd5d92b1473188af68051
SyRachid <syrachidsanogo78@gmail.com>
2023-08-30 00:19:09 +0000

    new commit for 2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module help to define a class of Square."""
+
+
+class Square:
+    """The class that define a Square."""
+    def __init__(self, size=0):
+        """constructor of  my class"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be in a integer")
+        if (size < 0):
+            raise ValueError("size must be >=0 ")
