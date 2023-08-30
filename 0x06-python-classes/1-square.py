e2f776a7a63dd04c63faa962b58987e661285ea2
SyRachid <syrachidsanogo78@gmail.com>
2023-08-30 00:04:43 +0000

    new commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module define a class Square with attribute"""
+
+
+class Square:
+    """This class is represent a square with attribute."""
+    def __init__(self, size):
+        """the constructor with size parameter"""
+        self.__size = size
+        """the first parameter is size of square."""
