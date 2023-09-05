8cb5f765616040b5355d63dbad3b8e4981f7add0
SyRachid <syrachidsanogo78@gmail.com>
2023-09-05 19:34:01 +0000

    commit for 4-print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/bin/usr/python3
+"""
+this module is to print a square which size given argument
+
+"""
+
+
+def print_square(size):
+    """print a square with given size"""
+    if (not isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    if (isinstance(size, float) and size < 0):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        ligne = "#" * size
+        print(ligne)
+
