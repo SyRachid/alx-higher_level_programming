71db8362a42bc70876e7c61717710391899af3db
SyRachid <syrachidsanogo78@gmail.com>
2023-09-11 20:53:11 +0000

    commit for 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 """ this module returns a list of available attributes and methods
     of an object"""
 
 
-#!/usr/bin/python3
 def lookup(obj):
     """return a list of attributes and methods of
     an object"""
